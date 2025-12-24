4f81d5188edc6c72617cc01bcc791affc18c6322
vivek <vivekbilla822@gmail.com>
2025-12-25 00:10:49 +0530

    Deploy InsightBoard frontend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+
 
 export const fetchOverview = async () => {
   const res = await fetch(`${BASE_URL}/metrics/overview`);
