dba6a0235641ac385ba67d4f34c84e9fb7ce764a
vivek <vivekbilla822@gmail.com>
2025-12-26 01:37:40 +0530

    frontend changes

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -4,21 +4,21 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 
 
 export const fetchOverview = async () => {
-  const res = await fetch(`${BASE_URL}/metrics/overview`);
+  const res = await fetch(`${BASE_URL}/api/metrics/overview`);
   return res.json();
 };
 
 export const fetchLogs = async () => {
-  const res = await fetch(`${BASE_URL}/logs?limit=10`);
+  const res = await fetch(`${BASE_URL}/api/logs?limit=10`);
   return res.json();
 };
 
 export const fetchTraffic = async () => {
-  const res = await fetch(`${BASE_URL}/charts/traffic`);
+  const res = await fetch(`${BASE_URL}/api/charts/traffic`);
   return res.json();
 };
 
 export const fetchErrors = async () => {
-  const res = await fetch(`${BASE_URL}/charts/errors`);
+  const res = await fetch(`${BASE_URL}/api/charts/errors`);
   return res.json();
 };
