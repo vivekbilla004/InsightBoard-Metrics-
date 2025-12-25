1557c08166819d8928365e7be01a376887ad9643
vivek <vivekbilla822@gmail.com>
2025-12-26 00:11:21 +0530

    Backend Changes

--- Backend/server.js
+++ Backend/server.js
@@ -32,7 +32,7 @@ app.set("io", io);
 // middlewares
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "Client_URL",
 }));
 
 app.use(requestLogger);
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
-
+// const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "http://localhost:5000/api";
 
 
 export const fetchOverview = async () => {
